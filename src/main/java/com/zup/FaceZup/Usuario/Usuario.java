ac8aa3e22fa0b896ab6b9283c4c3cde10f39fbed
leticia.reginajf <leticia.reginajf@gmail.com>
2021-08-30 23:34:14 -0300

    Atualizando a limpeza de código Usuario

--- src/main/java/com/zup/FaceZup/Usuario/Usuario.java
+++ src/main/java/com/zup/FaceZup/Usuario/Usuario.java
@@ -24,34 +24,42 @@ public class Usuario {
     }
 
     public String getNome() {
+
         return nome;
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public String getSobrenome() {
+
         return sobrenome;
     }
 
     public void setSobrenome(String sobrenome) {
+
         this.sobrenome = sobrenome;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getCargo() {
+
         return cargo;
     }
 
     public void setCargo(String cargo) {
+
         this.cargo = cargo;
     }
 
